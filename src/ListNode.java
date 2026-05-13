6c7f4745453fcd4c2751949d7dd52b22f8feff05
Viktor Huchiev <274128020+viktorHuc@users.noreply.github.com>
2026-05-13 11:19:06 -0700

    more work with Linked List

--- src/ListNode.java
+++ src/ListNode.java
@@ -2,4 +2,14 @@ public class ListNode {
     public char data;
     public ListNode next;
 
+    public ListNode(char data, ListNode next){
+        this.data = data;
+        this.next = next;
+    }
+
+    public ListNode(char data){
+        this.data = data;
+    }
+
+    public ListNode(){}
 }
--- src/Practice.java
+++ src/Practice.java
@@ -17,18 +17,21 @@ public class Practice {
 
         ListNode hello = new ListNode();
         hello.data = 'x';
+
+        //make sure everything is pointed before print it;
         extra.next = hello;
-
-
         myNode.next = aNode;
         aNode.next = moreNode;
         coolNode.next = myNode;
         //moreNode.next = coolNode;
 
+        //printList(coolNode);
 
-        //System.out.println(extra.data);
-        // System.out.println(aNode.data);
-        printList(coolNode);
+        ListNode newHead = removeAt(coolNode, 4);
+        System.out.println("--------------");
+        printList(newHead);
+        
+;
         System.out.println(countX(coolNode));
 
     }
@@ -45,11 +48,21 @@ public class Practice {
         ListNode current = head;
         int count = 0;
         while(current != null){
-            if(current.data == 'x'){
-                count++;
-            }
+            if(current.data == 'x'){count++;}
             current = current.next;
         }
         return count;
+    }
+
+    public static ListNode removeAt(ListNode head, int removeIndex){
+        ListNode current = head;
+        if(removeIndex == 0){
+            return current.next;
+        }
+        for (int i = 0; i < removeIndex -1; i++){
+            current = current.next; 
+        }
+        current.next = current.next.next;
+        return head;
     }
 }
